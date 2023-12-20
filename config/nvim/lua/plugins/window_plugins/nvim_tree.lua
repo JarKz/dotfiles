bf12ff92d18853ea7616ca30c9603a16dc39909b
Pavel <work.belyavsky.p.a@gmail.com>
2023-12-20 08:52:53 +0300

    feat:updated keymap for nvim-tree from rm to trash

--- config/nvim/lua/plugins/window_plugins/nvim_tree.lua
+++ config/nvim/lua/plugins/window_plugins/nvim_tree.lua
@@ -1,3 +1,13 @@
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
+  api.config.mappings.default_on_attach(bufnr)
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+  vim.keymap.set('n', 'd', api.fs.trash, opts('Trash'))
+end
+
 return {
   "nvim-tree/nvim-tree.lua",
   dependencies = {
@@ -5,6 +15,7 @@ return {
     "folke/which-key.nvim",
   },
   opts = {
+    on_attach = on_attach,
     disable_netrw = false,
     hijack_netrw = true,
     auto_reload_on_write = true,
