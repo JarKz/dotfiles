008c41c65fe2e73d21fc83458f0d94d564429c15
Pavel <work.belyavsky.p.a@gmail.com>
2024-04-07 16:04:37 +0300

    feat: added nixpkgs for macbook-nix device

---
+++ config/nixpkgs/overlays.nix
@@ -0,0 +1,4 @@
+[ 
+  (import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
+  (import (builtins.fetchTarball "https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz"))
+]
