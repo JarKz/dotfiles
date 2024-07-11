00a6ca7de76753130c8b6170b92478fcb3e0ed6b
Pavel <work.belyavsky.p.a@gmail.com>
2024-07-11 20:39:37 +0300

    fix: update highlights of nvim-notify

--- config/nvim/lua/plugins/window_plugins/notify.lua
+++ config/nvim/lua/plugins/window_plugins/notify.lua
@@ -1,3 +1,17 @@
+---@param link_from string the highlight which will be cleared and linked to hl destination
+---@param link_to string the highliht to which will be linked hl origin
+local function clear_and_link_to(link_from, link_to)
+  local notify_info_icon = vim.api.nvim_get_hl(0, { name = link_from })
+  notify_info_icon.fg = nil
+  notify_info_icon.link = link_to
+  vim.api.nvim_set_hl(0, link_from, notify_info_icon)
+end
+
+local function fix_notify_hightlights()
+  clear_and_link_to("NotifyINFOIcon", "green")
+  clear_and_link_to("NotifyINFOTitle", "green")
+end
+
 return {
   "rcarriga/nvim-notify",
   opts = {
@@ -15,5 +29,7 @@ return {
     local notify = require('notify')
     notify.setup(opts)
     vim.notify = notify
+
+    fix_notify_hightlights()
   end
 }
