f8396f4de32f3eee941068c7428cada5fa3987fa
Pavel <work.belyavsky.p.a@gmail.com>
2024-07-11 20:13:11 +0300

    refactor(nvim): update vim api from deprecated to recommended

--- config/nvim/lua/plugins/window_plugins/notify.lua
+++ config/nvim/lua/plugins/window_plugins/notify.lua
@@ -3,7 +3,7 @@ return {
   opts = {
     render = "compact",
     background_colour = function()
-      local hl = vim.api.nvim_get_hl_by_name("Normal", true)
+      local hl = vim.api.nvim_get_hl(0, { name = "Normal" })
       if hl.background == nil then
         return "#000000"
       end
