a93f485660040583c62571c8d25e40db5b205fd4
Pavel <work.belyavsky.p.a@gmail.com>
2024-07-20 23:36:23 +0300

    fix(nvim): add command alias to RipSubstituse using 'RipSub' name

--- config/nvim/lua/plugins/window_plugins/rip_substitute.lua
+++ config/nvim/lua/plugins/window_plugins/rip_substitute.lua
@@ -1,6 +1,8 @@
+vim.api.nvim_command("command RipSub RipSubstitute")
+
 return {
   "chrisgrieser/nvim-rip-substitute",
-  cmd = "RipSub",
+  cmd = "RipSubstitute",
   opts = {
     popupWin = {
       border = require("config.window_config").border,
