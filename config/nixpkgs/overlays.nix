f90ab27ba29d33b268c7c88bab437cf111926ed7
Pavel <work.belyavsky.p.a@gmail.com>
2024-12-02 10:29:47 +0300

    refactor: cleanup useless directories and files

--- config/nixpkgs/overlays.nix
+++
@@ -1,4 +0,0 @@
-[ 
-  (import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
-  (import (builtins.fetchTarball "https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz"))
-]
--- config/tmux/README.md
+++
@@ -1,5 +0,0 @@
-# Tmux
-
-For better understanding visit the [page](https://github.com/gpakosz/.tmux).
-
-Also I repeat, **do not change `tmux.conf` file!**
