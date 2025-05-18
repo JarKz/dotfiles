1fc5698a6b27b21412834a38ae0b7919960481a5
Pavel <work.belyavsky.p.a@gmail.com>
2025-05-18 23:55:42 +0300

    fix(nvim): remove custom cmdline blink configuration

--- config/nvim/lua/plugins/cmp/blink.lua
+++ config/nvim/lua/plugins/cmp/blink.lua
@@ -17,14 +17,6 @@ return {
       ["<C-u>"] = {"scroll_documentation_up"},
     },
 
-    cmdline = {
-      keymap = {
-        preset = 'enter',
-        ['<CR>'] = {},
-        ['<S-CR>'] = { 'accept' },
-      },
-    },
-
     appearance = {
       use_nvim_cmp_as_default = true,
       nerd_font_variant = 'mono'
